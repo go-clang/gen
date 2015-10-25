674bdb442b5d10485e5410bbd20258dee974c009
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-10-25 10:37:30 +0100

    Move the receiverName function

--- cmd/go-clang-generate/receiver.go
+++ cmd/go-clang-generate/receiver.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"unicode"
+)
+
 type Receiver struct {
 	Name          string
 	CName         string
 	Type          string
 	PrimitiveType string
+}
+
+func receiverName(s string) string {
+	var n []rune
+
+	for _, c := range s {
+		if unicode.IsUpper(c) {
+			n = append(n, unicode.ToLower(c))
+		}
+	}
+
+	return string(n)
 }
