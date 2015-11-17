28f062dff8bda5bda79a97a7ee791976cb222fab
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-11-17 20:07:22 +0100

    Add the Vagrant setup

---
+++ scripts/vagrant-environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export GO_VERSION=1.4.2
+
+# Install Go
+mkdir -p $HOME/go
+
+echo "Downloading Go:"
+wget -nv https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+tar -xf go${GO_VERSION}.linux-amd64.tar.gz -C $HOME/go
+rm go${GO_VERSION}.linux-amd64.tar.gz
+
+# Setup user
+echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+echo 'export GOROOT=$GOPATH/go' >> $HOME/.bashrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.bashrc
+echo 'cd $HOME/go/src/github.com/zimmski/go-clang-phoenix/' >> $HOME/.bashrc
+
+# TODO how can we load .bashrc at this point?
+export GOPATH=$HOME/go
+export GOROOT=$GOPATH/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+cd $HOME/go/src/github.com/zimmski/go-clang-phoenix/
+
+# Install go-clang
+make install-dependencies
+make install-tools
+make install
