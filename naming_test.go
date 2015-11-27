a5b0dc054032f97171ad29bf487c09092c5e90c1
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-11-27 21:55:44 +0100

    Simple testcase to make the CI happy because we do not have ANY tests yet...

---
+++ naming_test.go
@@ -0,0 +1,36 @@
+package generate
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUpperFirstCharacter(t *testing.T) {
+	for _, tc := range []struct {
+		Data   string
+		Expect string
+	}{
+		{
+			Data:   "a",
+			Expect: "A",
+		},
+		{
+			Data:   "A",
+			Expect: "A",
+		},
+		{
+			Data:   "abc",
+			Expect: "Abc",
+		},
+		{
+			Data:   "Abc",
+			Expect: "Abc",
+		},
+		{
+			Data:   "ABC",
+			Expect: "ABC",
+		},
+	} {
+		assert.Equal(t, tc.Expect, UpperFirstCharacter(tc.Data))
+	}
+}
