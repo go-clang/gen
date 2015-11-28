f68089a547e74a814f5664b4b46a646674806640
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-11-28 15:25:45 +0100

    Rewrite all repository mentions, especially imports

--- scripts/new-clang-version.sh
+++ scripts/new-clang-version.sh
@@ -32,6 +32,7 @@ make test || exit
 
 # Change versions in files
 sed -i -e "s/3.4/${LLVM_VERSION}/g" .travis.yml
+find . -type f -not -path '*/\.*' -exec sed -i -e "s/go-clang-phoenix-bootstrap/go-clang-phoenix-v${LLVM_VERSION}/g" {} +
 
 # Show the current state of the repository
 git status
