38783fcfa4d6251a88a6e27a1116c6fee834d719
Markus Zimmermann <markus.zimmermann@nethead.at>
2016-09-16 22:27:35 +0200

    Remove the symbolic link for libClang
    This is not needed anymore, or it was never needed?!

--- scripts/switch-clang-version.sh
+++ scripts/switch-clang-version.sh
@@ -14,7 +14,5 @@ sudo add-apt-repository --enable-source "deb http://llvm.org/apt/${CODENAME}/ ll
 sudo apt-get update
 
 sudo rm /usr/bin/llvm-config
-sudo rm /usr/lib/x86_64-linux-gnu/libclang.so
 sudo apt-get install -y clang-$LLVM_VERSION libclang1-$LLVM_VERSION libclang-$LLVM_VERSION-dev llvm-$LLVM_VERSION llvm-$LLVM_VERSION-dev llvm-$LLVM_VERSION-runtime libclang-common-$LLVM_VERSION-dev
 sudo ln -s /usr/bin/llvm-config-$LLVM_VERSION /usr/bin/llvm-config
-sudo ln -s /usr/lib/x86_64-linux-gnu/libclang-$LLVM_VERSION.so /usr/lib/x86_64-linux-gnu/libclang.so
