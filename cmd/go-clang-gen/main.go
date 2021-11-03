3f519e2257a88b901d0979edceb72fae00d1fb44
Koichi Shiraishi <zchee.io@gmail.com>
2021-11-03 16:51:29 +0900

    cmd/go-clang-gen: refactoring go-clang-gen
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/go-clang-gen/main.go
+++ cmd/go-clang-gen/main.go
@@ -24,12 +24,11 @@ func main() {
 	flag.Parse()
 
 	api := &gen.API{
-		PrepareFunctionName:     prepareFunctionName,
-		PrepareFunction:         prepareFunction,
-		FilterFunction:          filterFunction,
-		FilterFunctionParameter: filterFunctionParameter,
-		FixedFunctionName:       fixedFunctionName,
-
+		PrepareFunctionName:      prepareFunctionName,
+		PrepareFunction:          prepareFunction,
+		FilterFunction:           filterFunction,
+		FilterFunctionParameter:  filterFunctionParameter,
+		FixedFunctionName:        fixedFunctionName,
 		PrepareStructMembers:     prepareStructMembers,
 		FilterStructMemberGetter: filterStructMemberGetter,
 	}
@@ -38,11 +37,10 @@ func main() {
 		c := exec.Command("llvm-config", "--prefix")
 		prefix, err := c.CombinedOutput()
 		if err != nil {
-			var exitErr *exec.ExitError
-			if errors.As(err, &exitErr) {
+			if exitErr := new(exec.ExitError); errors.As(err, &exitErr) {
 				err = exitErr
 			}
-			fmt.Println(err)
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 
@@ -52,15 +50,13 @@ func main() {
 		}
 
 		if flagLLVMRoot == "" {
-			fmt.Println("couldn't parse LLVM root directory")
+			fmt.Fprintln(os.Stderr, "couldn't parse LLVM root directory")
 			os.Exit(1)
 		}
 	}
 
-	err := genclang.Cmd(flagLLVMRoot, api)
-	if err != nil {
-		fmt.Println(err)
-
+	if err := genclang.Cmd(flagLLVMRoot, api); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
@@ -102,8 +98,49 @@ func prepareFunctionName(g *gen.Generation, f *gen.Function) string {
 	return fname
 }
 
+func prepareFunctionName2(g *gen.Generation, f *gen.Function) string {
+	fname := f.Name
+	fname = strings.TrimPrefix(fname, "clang_")
+
+	var trimFuncNames = []string{
+		"indexLoc_",
+		"index_",
+		"Location_",
+		"Range_",
+		"remap_",
+	}
+	// trim some allowlisted prefixes by their function name
+	for _, name := range trimFuncNames {
+		idx := strings.LastIndex(fname, name)
+		if idx != -1 {
+			fname = fname[idx:]
+			break
+		}
+	}
+
+	// trim some allowlisted prefixes by their types
+	if len(f.Parameters) > 0 && g.IsEnumOrStruct(f.Parameters[0].Type.GoName) {
+		switch f.Parameters[0].Type.GoName {
+		case "CodeCompleteResults":
+			fname = strings.TrimPrefix(fname, "codeComplete")
+
+		case "CompletionString":
+			if f.CName == "clang_getNumCompletionChunks" {
+				fname = "NumChunks"
+			} else {
+				fname = strings.TrimPrefix(fname, "getCompletion")
+			}
+
+		case "SourceRange":
+			fname = strings.TrimPrefix(fname, "getRange")
+		}
+	}
+
+	return fname
+}
+
 func fixedFunctionName(f *gen.Function) string {
-	// Needs to be renamed manually since clang_getTranslationUnitCursor will conflict with clang_getCursor
+	// needs to be renamed manually since clang_getTranslationUnitCursor will conflict with clang_getCursor
 	if f.CName == "clang_getTranslationUnitCursor" {
 		return "TranslationUnitCursor"
 	}
@@ -179,27 +216,126 @@ func prepareFunction(f *gen.Function) {
 	}
 }
 
+func prepareFunction2(f *gen.Function) {
+	for i := range f.Parameters {
+		p := &f.Parameters[i]
+
+		if f.CName == "clang_getRemappingsFromFileList" {
+			switch p.CName {
+			case "filePaths":
+				p.Type.IsSlice = true
+
+			case "numFiles":
+				p.Type.LengthOfSlice = "filePaths"
+			}
+
+			continue
+		}
+
+		// allowflag types that are return arguments
+		switch p.Type.PointerLevel {
+		case 1:
+			switch p.Type.GoName {
+			case gen.GoInt32,
+				gen.GoUInt32,
+				"File",
+				"FileUniqueID",
+				"IdxClientFile",
+				"cxstring",
+				"CompilationDatabase_Error",
+				"PlatformAvailability",
+				"SourceRange",
+				"LoadDiag_Error":
+
+				p.Type.IsReturnArgument = true
+			}
+
+		case 2:
+			switch p.Type.GoName {
+			case "Token",
+				"Cursor":
+
+				p.Type.IsReturnArgument = true
+			}
+		}
+
+		if f.CName == "clang_disposeOverriddenCursors" && p.CName == "overridden" {
+			p.Type.IsSlice = true
+		}
+
+		// if this is an array length parameter we need to find its partner
+		paCName := gen.ArrayNameFromLength(p.CName)
+
+		if paCName != "" {
+			for j := range f.Parameters {
+				pa := &f.Parameters[j]
+
+				if strings.EqualFold(pa.CName, paCName) {
+					switch {
+					case pa.Type.CGoName == gen.CSChar && pa.Type.PointerLevel == 2:
+						// nothing to do
+
+					case pa.Type.GoName == "CompletionResult":
+						// nothing to do
+
+					case pa.Type.GoName == "Token":
+						// nothing to do
+
+					case pa.Type.GoName == "Cursor":
+						// nothing to do
+
+					case pa.Type.GoName == "struct CXUnsavedFile" || pa.Type.GoName == "UnsavedFile":
+						pa.Type.GoName = "UnsavedFile"
+						pa.Type.CGoName = "struct_CXUnsavedFile"
+
+					default:
+						break
+					}
+
+					p.Type.LengthOfSlice = pa.Name
+					pa.Type.IsSlice = true
+
+					if pa.Type.IsReturnArgument && p.Type.PointerLevel > 0 {
+						p.Type.IsReturnArgument = true
+					}
+
+					break
+				}
+			}
+		}
+	}
+
+	for i := range f.Parameters {
+		p := &f.Parameters[i]
+
+		if p.Type.CGoName == gen.CSChar && p.Type.PointerLevel == 2 && !p.Type.IsSlice {
+			p.Type.IsReturnArgument = true
+		}
+	}
+}
+
 func filterFunction(f *gen.Function) bool {
-	// Some functions are not compiled in the library see https://lists.launchpad.net/desktop-packages/msg75835.html for a never resolved bug report
-	if f.CName == "clang_CompileCommand_getMappedSourceContent" || f.CName == "clang_CompileCommand_getMappedSourcePath" || f.CName == "clang_CompileCommand_getNumMappedSources" {
-		fmt.Printf("Ignore function %q because it is not compiled within libClang\n", f.CName)
+	switch f.CName {
+	case "clang_CompileCommand_getMappedSourceContent", "clang_CompileCommand_getMappedSourcePath", "clang_CompileCommand_getNumMappedSources":
+		// some functions are not compiled in the library see https://lists.launchpad.net/desktop-packages/msg75835.html for a never resolved bug report
+		fmt.Fprintf(os.Stderr, "Ignore function %q because it is not compiled within libClang\n", f.CName)
+
+		return false
+
+	case "clang_executeOnThread", "clang_getInclusions":
+		// some functions can not be handled automatically by us
+		fmt.Fprintf(os.Stderr, "Ignore function %q because it cannot be handled automatically\n", f.CName)
+
+		return false
+
+	case "clang_annotateTokens", "clang_getCursorPlatformAvailability", "clang_visitChildren":
+		// some functions are simply manually implemented
+		fmt.Fprintf(os.Stderr, "Ignore function %q because it is manually implemented\n", f.CName)
 
 		return false
 	}
-	// Some functions can not be handled automatically by us
-	if f.CName == "clang_executeOnThread" || f.CName == "clang_getInclusions" {
-		fmt.Printf("Ignore function %q because it cannot be handled automatically\n", f.CName)
 
-		return false
-	}
-	// Some functions are simply manually implemented
-	if f.CName == "clang_annotateTokens" || f.CName == "clang_getCursorPlatformAvailability" || f.CName == "clang_visitChildren" {
-		fmt.Printf("Ignore function %q because it is manually implemented\n", f.CName)
-
-		return false
-	}
-
-	// TODO if this function is from CXString.h we ignore it https://github.com/go-clang/gen/issues/25
+	// TODO(go-clang): if this function is from CXString.h we ignore it https://github.com/go-clang/gen/issues/25
 	for i := range f.IncludeFiles {
 		if strings.HasSuffix(i, "CXString.h") {
 			return false
@@ -210,9 +346,25 @@ func filterFunction(f *gen.Function) bool {
 }
 
 func filterFunctionParameter(p gen.FunctionParameter) bool {
-	// These pointers are ok
-	if p.Type.PointerLevel == 1 && (p.Type.CGoName == gen.CSChar || p.Type.GoName == "UnsavedFile" || p.Type.GoName == "CodeCompleteResults" || p.Type.GoName == "CursorKind" || p.Type.GoName == "IdxContainerInfo" || p.Type.GoName == "IdxDeclInfo" || p.Type.GoName == "IndexerCallbacks" || p.Type.GoName == "TranslationUnit" || p.Type.GoName == "IdxEntityInfo" || p.Type.GoName == "IdxAttrInfo") {
-		return false
+	// these pointers are ok
+	if p.Type.PointerLevel == 1 {
+		if p.Type.CGoName == gen.CSChar {
+			return false
+		}
+
+		switch p.Type.GoName {
+		case "UnsavedFile",
+			"CodeCompleteResults",
+			"CursorKind",
+			"IdxContainerInfo",
+			"IdxDeclInfo",
+			"IndexerCallbacks",
+			"TranslationUnit",
+			"IdxEntityInfo",
+			"IdxAttrInfo":
+
+			return false
+		}
 	}
 
 	return true
@@ -224,7 +376,7 @@ func prepareStructMembers(s *gen.Struct) {
 			m.Type.GoName = gen.GoBool
 		}
 
-		// If this is an array length parameter we need to find its partner
+		// if this is an array length parameter we need to find its partner
 		maCName := gen.ArrayNameFromLength(m.CName)
 
 		if maCName != "" {
@@ -232,7 +384,10 @@ func prepareStructMembers(s *gen.Struct) {
 				if strings.ToLower(ma.CName) == strings.ToLower(maCName) {
 					m.Type.LengthOfSlice = ma.CName
 					ma.Type.IsSlice = true
-					ma.Type.LengthOfSlice = m.CName // TODO wrong usage but needed for the getter generation... maybe refactor this LengthOfSlice alltogether? https://github.com/go-clang/gen/issues/49
+					// TODO(go-clang): wrong usage but needed for the getter generation...
+					// maybe refactor this LengthOfSlice alltogether?
+					// https://github.com/go-clang/gen/issues/49
+					ma.Type.LengthOfSlice = m.CName
 
 					break
 				}
@@ -243,7 +398,8 @@ func prepareStructMembers(s *gen.Struct) {
 	prepareStructMembersArrayStruct(s)
 }
 
-// prepareStructMembersArrayStruct checks if the struct has two member variables, one is an array and the other a plain int/uint with size/length/count/len as its name because then this should be an array struct, and we connect them to handle a slice.
+// prepareStructMembersArrayStruct checks if the struct has two member variables, one is an array and the other a plain int/uint with
+// size/length/count/len as its name because then this should be an array struct, and we connect them to handle a slice.
 func prepareStructMembersArrayStruct(s *gen.Struct) {
 	if len(s.Members) != 2 {
 		return
@@ -253,9 +409,13 @@ func prepareStructMembersArrayStruct(s *gen.Struct) {
 		return
 	}
 
-	// If one of the members is already marked as array/slice another heuristic has already covered both members.
-	if s.Members[0].Type.IsArray || s.Members[1].Type.IsArray ||
-		s.Members[0].Type.IsSlice || s.Members[1].Type.IsSlice {
+	// if one of the members is already marked as array/slice another heuristic has already covered both members.
+	switch {
+	case s.Members[0].Type.IsArray,
+		s.Members[1].Type.IsArray,
+		s.Members[0].Type.IsSlice,
+		s.Members[1].Type.IsSlice:
+
 		return
 	}
 
@@ -271,25 +431,26 @@ func prepareStructMembersArrayStruct(s *gen.Struct) {
 	}
 
 	lengthName := strings.ToLower(c.CName)
-	if lengthName != "count" && lengthName != "len" && lengthName != "length" && lengthName != "size" {
+	if lengthName != "count" &&
+		lengthName != "len" && lengthName != "length" && lengthName != "size" {
+
 		return
 	}
 
 	c.Type.LengthOfSlice = a.CName
 	a.Type.IsSlice = true
-	a.Type.LengthOfSlice = c.CName // TODO wrong usage but needed for the getter generation... maybe refactor this LengthOfSlice alltogether? https://github.com/go-clang/gen/issues/49
+	// TODO(go-clang): wrong usage but needed for the getter generation...
+	// maybe refactor this LengthOfSlice alltogether?
+	// https://github.com/go-clang/gen/issues/49
+	a.Type.LengthOfSlice = c.CName
 }
 
-func arrayLengthCombination(a *gen.Type, b *gen.Type) bool {
-	return a.PointerLevel == 1 && b.PointerLevel == 0 &&
-		!gen.IsInteger(a) && gen.IsInteger(b)
+func arrayLengthCombination(x *gen.Type, y *gen.Type) bool {
+	return x.PointerLevel == 1 && y.PointerLevel == 0 &&
+		!gen.IsInteger(x) && gen.IsInteger(y)
 }
 
 func filterStructMemberGetter(m *gen.StructMember) bool {
-	// We do not want getters to *int_data members
-	if strings.HasSuffix(m.CName, "int_data") {
-		return false
-	}
-
-	return true
+	// we do not want getters to *int_data members
+	return !strings.HasSuffix(m.CName, "int_data")
 }
